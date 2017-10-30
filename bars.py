4592e5f17daa3aba7ce3d30fc6794d4637cf7631
Malemute <erlinn@fromru.com>
2017-10-30 16:24:27 +0300

    First draft

--- bars.py
+++ bars.py
@@ -1,8 +1,10 @@
 import json
+from geopy.distance import vincenty
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as the_file:
+        return json.load(the_file)
 
 
 def get_biggest_bar(data):
@@ -13,9 +15,18 @@ def get_smallest_bar(data):
     pass
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(bars_list, longitude, latitude):
+    
     pass
 
 
 if __name__ == '__main__':
-    pass
+    filepath = sys.argv[1]
+    parsed_json = load_data(filepath)
+    bars_list = parsed_json['features']
+	smallest_bar = get_smallest_bar(bars_list)
+	biggest_bar = get_biggest_bar(bars_list)
+	
+	my_coordinates = input("Longitude, Latitude: ")
+	closest_bar = get_closest_bar(bars_list, my_coordinates[0], my_coordinates[1])
+    print("")
