40be25b7804ac796f6557b408725f48898ccfcbb
Malemute <erlinn@fromru.com>
2017-11-01 22:52:21 +0300

    Text mostly done

--- bars.py
+++ bars.py
@@ -7,26 +7,36 @@ def load_data(filepath):
         return json.load(the_file)
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(bars_list):
+    biggest_bar = max(bars_list,
+                      lambda index: index['properties']['Attributes']['SeatsCount'])
+    return biggest_bar
 
 
-def get_smallest_bar(data):
-    pass
+def get_smallest_bar(bars_list):
+    smallest_bar = min(bars_list,
+                       lambda index: index['properties']['Attributes']['SeatsCount'])
+    return smallest_bar
 
 
-def get_closest_bar(bars_list, longitude, latitude):
-    
-    pass
+def get_distance(first_point, second_point):
+    return vincenty(first_point, second_point)
+
+
+def get_closest_bar(bars_list, coordinates):
+    closest_bar = min(bars_list, key=get_distance(coordinates,
+                        lambda index: index['geometry']['coordinates']))
+    return closest_bar
 
 
 if __name__ == '__main__':
     filepath = sys.argv[1]
     parsed_json = load_data(filepath)
     bars_list = parsed_json['features']
-	smallest_bar = get_smallest_bar(bars_list)
-	biggest_bar = get_biggest_bar(bars_list)
-	
-	my_coordinates = input("Longitude, Latitude: ")
-	closest_bar = get_closest_bar(bars_list, my_coordinates[0], my_coordinates[1])
-    print("")
+    biggest_bar = get_biggest_bar(bars_list)
+    smallest_bar = get_smallest_bar(bars_list)
+
+    my_coordinates = map(float, input("Longitude, Latitude: ").split(","))
+    closest_bar = get_closest_bar(bars_list, my_coordinates)
+
+    print("The biggest bar is {} at {}".format())
